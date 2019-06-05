90ea113393af775739c1b638c356edca51a8a8ec
Garrett Halstein <garrett.halstein@emory.edu>
2019-06-05 11:41:48 -0400

    initialize of Person and some attr

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,13 @@
 # your code goes here
+require 'pry'
+class Person
+  def initialize (name)
+  	@name = name
+  	@account = 25
+  	@happiness = 8
+  	@hygiene = 8
+  end
+  attr_reader = :name, :account, :happiness, :hygiene
+  attr_writer = :happiness, :hygiene
+end
+binding.pry
